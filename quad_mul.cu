d147358a2be989a202c40ace24705b96acd57846
Arunesh Mishra <aruneshm@gmail.com>
2019-06-19 16:47:53 -0700

    addition

--- quad_mul.cu
+++ quad_mul.cu
@@ -21,6 +21,12 @@ typedef struct {
   cgbn_mem_t<BITS> mul_hi;
 } my_instance_t;
 
+typedef struct {
+  cgbn_mem_t<BITS> x;
+  cgbn_mem_t<BITS> y;
+  cgbn_mem_t<BITS> m;
+  cgbn_mem_t<BITS> result;
+} add_instance_t;
 
 typedef cgbn_context_t<TPI>         context_t;
 typedef cgbn_env_t<context_t, 768> env1024_t;
@@ -69,6 +75,31 @@ __device__
   #endif
 }
 
+__global__ void add_kernel(add_instance_t *problem_instances, uint32_t instance_count) {
+  context_t         bn_context;                                 // create a CGBN context
+  env1024_t         bn1024_env(bn_context);                     // construct a bn environment for 1024 bit math
+  env1024_t::cgbn_t a, b, m;                      // three 1024-bit values (spread across a warp)
+  env1024_t::cgbn_t res, res1;
+  // uint32_t np0;
+  
+  int32_t my_instance=(blockIdx.x*blockDim.x + threadIdx.x)/TPI;  // determine my instance number
+  
+  if(my_instance>=instance_count) return;                         // return if my_instance is not valid
+  
+  cgbn_load(bn1024_env, a, &(problem_instances[my_instance]).x);
+  cgbn_load(bn1024_env, b, &(problem_instances[my_instance]).y);
+  cgbn_load(bn1024_env, m, &(problem_instances[my_instance]).m);
+
+  cgbn_add(bn1024_env, res1, a, b);
+  if (cgbn_compare(bn1024_env, res1, m) >= 0) {
+       cgbn_sub(bn1024_env, res, res1, m);
+    } else {
+       cgbn_set(bn1024_env, res, res1); 
+    }
+
+  cgbn_store(bn1024_env, &(problem_instances[my_instance].result), res);
+}
+
 __global__ void my_kernel(my_instance_t *problem_instances, uint32_t instance_count) {
   context_t         bn_context;                                 // create a CGBN context
   env1024_t         bn1024_env(bn_context);                     // construct a bn environment for 1024 bit math
@@ -114,6 +145,56 @@ void print_uint8_array(uint8_t* array, int size) {
         printf("%02x", array[i]);
     }
     printf("\n");
+}
+
+std::vector<uint8_t*>* compute_addcuda(std::vector<uint8_t*> a, std::vector<uint8_t*> b, uint8_t* input_m_base, int num_bytes) {
+  int num_elements = a.size();
+
+  add_instance_t *gpuInstances;
+  add_instance_t* instance_array = (add_instance_t*) malloc(sizeof(add_instance_t) * num_elements);
+  cgbn_error_report_t *report;
+
+  // create a cgbn_error_report for CGBN to report back errors
+  NEW_CUDA_CHECK(cgbn_error_report_alloc(&report));
+  for (int i = 0; i < num_elements; i ++) {
+    std::memcpy((void*)instance_array[i].x._limbs, (const void*) a[i], num_bytes);
+    std::memcpy((void*)instance_array[i].y._limbs, (const void*) b[i], num_bytes);
+    std::memcpy((void*)instance_array[i].m._limbs, (const void*) input_m_base, num_bytes);
+  }
+
+  printf("Copying instances to the GPU ...\n");
+  NEW_CUDA_CHECK(cudaSetDevice(0));
+  NEW_CUDA_CHECK(cudaMalloc((void **)&gpuInstances, sizeof(add_instance_t)*num_elements));
+  NEW_CUDA_CHECK(cudaMemcpy(gpuInstances, instance_array, sizeof(add_instance_t)*num_elements, cudaMemcpyHostToDevice));
+  
+  int tpb = TPB;
+  printf("\n Threads per block =%d", tpb);
+  int IPB = TPB/TPI;
+  int tpi = TPI;
+  printf("\n Threads per instance = %d", tpi);
+  printf("\n Instances per block = %d", IPB);
+
+  uint32_t num_blocks = (num_elements+IPB-1)/IPB;
+  printf("\n Number of blocks = %d", num_blocks);
+
+  add_kernel<<<num_blocks, TPB>>>(gpuInstances, num_elements);
+  NEW_CUDA_CHECK(cudaDeviceSynchronize());
+  CGBN_CHECK(report);
+
+  // copy the instances back from gpuMemory
+  printf("Copying results back to CPU ...\n");
+  NEW_CUDA_CHECK(cudaMemcpy(instance_array, gpuInstances, sizeof(add_instance_t)*num_elements, cudaMemcpyDeviceToHost));
+
+  std::vector<uint8_t*>* res_vector = new std::vector<uint8_t*>();
+  for (int i = 0; i < num_elements; i ++) {
+     uint8_t* result = (uint8_t*) malloc(num_bytes * sizeof(uint8_t));
+     std::memcpy((void*)result, (const void*)instance_array[i].result._limbs, num_bytes);
+     res_vector->emplace_back(result);
+  }
+
+  free(instance_array);
+  cudaFree(gpuInstances);
+  return res_vector;
 }
 
 std::vector<uint8_t*>* compute_newcuda(std::vector<uint8_t*> a, std::vector<uint8_t*> b, uint8_t* input_m_base, int num_bytes, uint64_t inv) {
@@ -186,7 +267,6 @@ std::vector<uint8_t*>* compute_newcuda(std::vector<uint8_t*> a, std::vector<uint
   return res_vector;
 }
 
-
 std::pair<std::vector<uint8_t*>, std::vector<uint8_t*> > 
 compute_quadex_cuda(std::vector<uint8_t*> x0_a0,
                     std::vector<uint8_t*> x0_a1,
@@ -194,6 +274,20 @@ compute_quadex_cuda(std::vector<uint8_t*> x0_a0,
                     std::vector<uint8_t*> y0_a1,
                     uint8_t* input_m_base, int num_bytes, uint64_t inv) {
   int num_elements = x0_a0.size();
+  std::vector<uint8_t*>* x0_y0;
+  std::vector<uint8_t*>* x0_y1;
+  std::vector<uint8_t*>* x1_y0;
+  std::vector<uint8_t*>* x1_y1;
+// Logic: 
+//  var x0_y0 = fq_mul(x.a0, y.a0);
+//  var x1_y1 = fq_mul(x.a1, y.a1);
+//  var x1_y0 = fq_mul(x.a1, y.a0);
+//  var x0_y1 = fq_mul(x.a0, y.a1);
+//  return {
+//    a0: fq_add(a0_b0, fq_mul(a1_b1, alpha)),
+//    a1: fq_add(a1_b0, a0_b1)
+//  };
+//
   std::pair<std::vector<uint8_t*>, std::vector<uint8_t*> > res;
   return res;
 }
